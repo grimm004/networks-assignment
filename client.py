489e05c2617c261ad31861b051a81c5a87641519
grimm004 <grimm004@gmail.com>
2019-11-05 00:40:34 +0000

    Nearly completed coursework.

--- client.py
+++ client.py
@@ -1,15 +1,80 @@
 import socket
 import sys
+import json
 
+socket.setdefaulttimeout(10)
 endpoint = sys.argv[1] if len(sys.argv) == 3 else "127.0.0.1", int(sys.argv[2]) if len(sys.argv) > 2 else (int(sys.argv[1]) if len(sys.argv) == 2 else 8000)
 
+def encode_command(command, **kwargs):
+    kwargs["command"] = command
+    return json.dumps(kwargs).encode()
+
+
+def send_command(sock, command, **kwargs):
+    sock.send(encode_command(command, **kwargs))
+
+
+def await_response(sock):
+    return json.loads(sock.recv(1024).decode("utf-8"))
+
+
+def run_command(command, **kwargs):
+    with socket.socket() as sock:
+        sock.connect(endpoint)
+        send_command(sock, command, **kwargs)
+        return await_response(sock)
+
+
+def get_boards():
+    return run_command("GET_BOARDS")
+
+
+def get_messages(board_name):
+    return run_command("GET_MESSAGES", board_name = board_name)
+
+
+def post_message(board_name, message_title, message):
+    run_command("POST_MESSAGE", board_name = board_name, message_title = message_title, message = message)
+
+
 try:
-    print("Connecting to %s:%d" % endpoint)
-    sock = socket.socket()
-    sock.connect(endpoint)
-    print("Connected to server.")
+    boards = get_boards()
+    print("\n".join(["%d. %s" % (i + 1, boards[i]) for i in range(len(boards))]))
+    print("Available Commands: '<board number>', 'POST' or 'QUIT'.")
+    while True:
+        request = input("> ")
+        if request.isdigit():
+            selected = int(request)
+            if 0 < selected <= len(boards):
+                messages = get_messages(boards[selected - 1])
+                if len(messages) == 0:
+                    print("No messages in %s." % boards[selected - 1])
+                else:
+                    print("Messages in %s:" % boards[selected - 1])
+                    print("\n".join("%s: %s" % (message["message_title"], message["message"]) for message in messages))
+            else:
+                print("Invalid board selection.")
+                continue
+        elif request == "POST":
+            request = input("Enter board number:\n> ")
+            if request.isdigit():
+                selected = int(request)
+                if 0 < selected <= len(boards):
+                    post_message(boards[selected - 1], input("Enter message title:\n> "), input("Eneter message:\n> "))
+                else:
+                    print("Invalid board selection.")
+                    continue
+            else:
+                print("Invalid board selection.")
+        elif request == "QUIT":
+            break
+        elif request == "":
+            continue
+        else:
+            print("Invalid selection.\nAvailable Commands: '<board number>', 'POST' or 'QUIT'.")
 except ConnectionRefusedError:
-    print("Failed to connect to server.")
+    print("Error: Failed to connect to server.")
+except socket.timeout:
+    print("Error: Socket timed out")
 finally:
-    print("Disconnected from server.")
-    sock.close()
+    print("Exiting...")
--- server.py
+++ server.py
@@ -1,15 +1,77 @@
 import socket
 import sys
+import os
+from glob import glob
+from datetime import datetime
+import json
+
+BOARD_DIR = "board"
+LOG_FILE = "server.log"
+
+
+def log(address, command, success):
+    with open(LOG_FILE, "at+") as file:
+        file.write("%s\t%s\t%s\t%s\n" % ("%s:%d" % address, datetime.now().strftime("%d/%m/%Y %H:%M:%S"), command, "OK" if success else "Error"))
 
 source = sys.argv[1] if len(sys.argv) == 3 else "127.0.0.1", int(sys.argv[2]) if len(sys.argv) > 2 else (int(sys.argv[1]) if len(sys.argv) == 2 else 8000)
 
-print("Listening on %s:%d" % source)
+def get_boards():
+    return [path.split("\\")[-1].replace("_", " ") for path in glob(os.path.join(BOARD_DIR, "*"))]
 
-sock = socket.socket()
-sock.bind(source)
-sock.listen(5)
 
-while True:
-    a, b = sock.accept()
-    print("Received connection")
+def get_message(message_path):
+    with open(message_path, "rt") as file:
+        return file.read()
 
+
+def get_messages(board_name):
+    return [{"message_title": "-".join(message_path.split("\\")[-1].split("-")[2:]).replace("_", " "), "message": get_message(message_path)} for message_path in glob(os.path.join(BOARD_DIR, board_name.replace(" ", "_"), "*"))]
+
+
+def create_message(board_name, message_title, message):
+    board_path = os.path.join(BOARD_DIR, board_name.replace(" ", "_"))
+    if not os.path.exists(board_path):
+        os.makedirs(board_path)
+
+    message_filename = "%s-%s" % (datetime.now().strftime("%d%m%Y-%H%M%S"), message_title.replace(" ", "_"))
+    message_path = os.path.join(board_path, message_filename)
+
+    with open(message_path, "wt") as file:
+        file.write(message)
+
+
+def handle_request(request):
+    response = ""
+    if request["command"] == "GET_BOARDS":
+        response = json.dumps(get_boards())
+    elif request["command"] == "GET_MESSAGES":
+        messages = get_messages(request["board_name"])[-100:]
+        response = json.dumps(messages)
+    elif request["command"] == "POST_MESSAGE":
+        create_message(request["board_name"], request["message_title"], request["message"])
+        response = "[]"
+    else:
+        return None
+    return response
+
+try:
+    if not os.path.exists(BOARD_DIR):
+        os.makedirs(BOARD_DIR)
+
+    print("Listening on %s:%d" % source)
+    sock = socket.socket()
+    sock.bind(source)
+    sock.listen(5)
+
+    while True:
+        connection, address = sock.accept()
+        print("Received connection from %s%d." % address)
+        request = json.loads(connection.recv(1024).decode("utf-8"))
+        response = handle_request(request)
+        if response is not None:
+            connection.send(response.encode("utf-8"))
+        log(address, request["command"], response is not None)
+        connection.close()
+
+except OSError:
+    print("Error: Port %d already in use." % source[1])
