45417e0b732e04d5d68da1885de7c27f6c9ec1ec
grimm004 <grimm004@gmail.com>
2019-11-07 18:10:23 +0000

    Commented code

--- client.py
+++ client.py
@@ -2,79 +2,130 @@ import socket
 import sys
 import json
 
+# Set the default timeout of any created sockets to 10 seconds (as required)
 socket.setdefaulttimeout(10)
+# Access the supplied command line arguments (if one is supplied treat it as a port, if two are supplied treat as host and port) default to 127.0.0.1:8000
 endpoint = sys.argv[1] if len(sys.argv) == 3 else "127.0.0.1", int(sys.argv[2]) if len(sys.argv) > 2 else (int(sys.argv[1]) if len(sys.argv) == 2 else 8000)
 
 def encode_command(command, **kwargs):
+    # Encode a command with parameters into JSON
+    # Take the keyword args supplied as a dictionary and add the command name to it
     kwargs["command"] = command
-    return json.dumps(kwargs).encode()
+    # Encode and return the utf-8 encoded JSON
+    return json.dumps(kwargs).encode("utf-8")
 
 
 def send_command(sock, command, **kwargs):
+    # Encode the supplied command and arguments and send it
     sock.send(encode_command(command, **kwargs))
 
 
 def await_response(sock):
-    return json.loads(sock.recv(1024).decode("utf-8"))
+    try:
+        # Decode the received data and try deserialise and return it
+        return json.loads(sock.recv(1024).decode("utf-8"))
+    except ValueError:
+        # A value error means the JSON decoder encountered a corrupt or empty string
+        return None
 
 
 def run_command(command, **kwargs):
+    # Open a TCP socket
     with socket.socket() as sock:
+        # Connect to the supplied host and port
         sock.connect(endpoint)
+        # Send the supplied command
         send_command(sock, command, **kwargs)
+        # Once a response has been recieved, return it
         return await_response(sock)
 
 
 def get_boards():
+    # Run the GET_BOARDS command and return the result
     return run_command("GET_BOARDS")
 
 
 def get_messages(board_name):
+    # Run the GET_MESSAGES command and return the result
     return run_command("GET_MESSAGES", board_name = board_name)
 
 
 def post_message(board_name, message_title, message):
-    run_command("POST_MESSAGE", board_name = board_name, message_title = message_title, message = message)
+    # Run the POST_MESSAGE command and return the result
+    return run_command("POST_MESSAGE", board_name = board_name, message_title = message_title, message = message)
 
 
-try:
-    boards = get_boards()
-    print("\n".join(["%d. %s" % (i + 1, boards[i]) for i in range(len(boards))]))
-    print("Available Commands: '<board number>', 'POST' or 'QUIT'.")
-    while True:
-        request = input("> ")
-        if request.isdigit():
-            selected = int(request)
-            if 0 < selected <= len(boards):
-                messages = get_messages(boards[selected - 1])
-                if len(messages) == 0:
-                    print("No messages in %s." % boards[selected - 1])
-                else:
-                    print("Messages in %s:" % boards[selected - 1])
-                    print("\n".join("%s: %s" % (message["message_title"], message["message"]) for message in messages))
-            else:
-                print("Invalid board selection.")
-                continue
-        elif request == "POST":
-            request = input("Enter board number:\n> ")
+if __name__ == "__main__":
+    try:
+        # Fetch the list of available boards
+        boards = get_boards()
+        # If there is an error collecting the boards output an error and exit
+        if boards is None:
+            print("Error fetching boards.")
+            exit()
+        # Output the formatted list of boards
+        print("\n".join(["%d. %s" % (i + 1, boards[i]) for i in range(len(boards))]))
+        print("Available Commands: '<board number>', 'POST' or 'QUIT'.")
+        # Infinately loop to continuously handle user requests
+        while True:
+            # Fetch the users input
+            request = input("> ")
+            # If the input is a non-negative number
             if request.isdigit():
+                # Cast it from a string to an integer
                 selected = int(request)
+                # Check its within the range of board numbers
                 if 0 < selected <= len(boards):
-                    post_message(boards[selected - 1], input("Enter message title:\n> "), input("Eneter message:\n> "))
+                    # Get the messages for the corresponding board
+                    messages = get_messages(boards[selected - 1])
+                    # If the messages could not be retrieved print an error
+                    if messages is None:
+                        print("Error fetching messages.")
+                        continue
+                    # If there are no messages, output a warning
+                    if len(messages) == 0:
+                        print("No messages in %s." % boards[selected - 1])
+                    else:
+                        # If there are messages, output them
+                        print("Messages in %s:" % boards[selected - 1])
+                        print("\n".join("%s: %s" % (message["message_title"], message["message"]) for message in messages))
                 else:
                     print("Invalid board selection.")
                     continue
+            # If the input is a POST request
+            elif request == "POST":
+                # Request the user to input the desired board to post to
+                request = input("Enter board number:\n> ")
+                # Check its a valid board ID (as done above)
+                if request.isdigit():
+                    selected = int(request)
+                    if 0 < selected <= len(boards):
+                        # If all is valid, fetch message title and message input and post it to the server
+                        if post_message(boards[selected - 1], input("Enter message title:\n> "), input("Eneter message:\n> ")) is None:
+                            print("Error posting message.")
+                            continue
+                        else:
+                            print("Message posted.")
+                    else:
+                        print("Invalid board selection.")
+                        continue
+                else:
+                    print("Invalid board selection.")
+            # If the input is a QUIT request
+            elif request == "QUIT":
+                # Leave the while loop
+                break
+            elif request == "":
+                continue
             else:
-                print("Invalid board selection.")
-        elif request == "QUIT":
-            break
-        elif request == "":
-            continue
-        else:
-            print("Invalid selection.\nAvailable Commands: '<board number>', 'POST' or 'QUIT'.")
-except ConnectionRefusedError:
-    print("Error: Failed to connect to server.")
-except socket.timeout:
-    print("Error: Socket timed out")
-finally:
-    print("Exiting...")
+                print("Invalid selection.\nAvailable Commands: '<board number>', 'POST' or 'QUIT'.")
+    except ConnectionRefusedError:
+        print("Error: Failed to connect to server.")
+    except socket.timeout:
+        # Output an error if the socket times out and quit
+        print("Error: Socket timed out")
+    except ConnectionResetError:
+        # Output an error if the socket cannot connect and quit
+        print("Error: Connection reset")
+    finally:
+        print("Exiting...")
--- server.py
+++ server.py
@@ -10,68 +10,106 @@ LOG_FILE = "server.log"
 
 
 def log(address, command, success):
+    # Open the log file for text appending
     with open(LOG_FILE, "at+") as file:
+        # Write the required information to the log file
         file.write("%s\t%s\t%s\t%s\n" % ("%s:%d" % address, datetime.now().strftime("%d/%m/%Y %H:%M:%S"), command, "OK" if success else "Error"))
 
+# Access the supplied command line arguments (if one is supplied treat it as a port, if two are supplied treat as host and port) default to 127.0.0.1:8000
 source = sys.argv[1] if len(sys.argv) == 3 else "127.0.0.1", int(sys.argv[2]) if len(sys.argv) > 2 else (int(sys.argv[1]) if len(sys.argv) == 2 else 8000)
 
+
 def get_boards():
+    # Use list comprehention and string manipulation to extract the name of each board from the list of file names in the board directory
     return [path.split("\\")[-1].replace("_", " ") for path in glob(os.path.join(BOARD_DIR, "*"))]
 
 
 def get_message(message_path):
+    # Open the message file for text reading
     with open(message_path, "rt") as file:
+        # Read and return the contents
         return file.read()
 
 
 def get_messages(board_name):
+    # Use list comprehention and string manipulation to extract the data for the last 100 messages from the files in the boards directory
     return [{"message_title": "-".join(message_path.split("\\")[-1].split("-")[2:]).replace("_", " "), "message": get_message(message_path)} for message_path in glob(os.path.join(BOARD_DIR, board_name.replace(" ", "_"), "*"))]
 
 
 def create_message(board_name, message_title, message):
+    # Access the path of the board being posted to
     board_path = os.path.join(BOARD_DIR, board_name.replace(" ", "_"))
+    # Create the path if it does not exist
     if not os.path.exists(board_path):
         os.makedirs(board_path)
 
+    # Access the filename and path of the message to be posted
     message_filename = "%s-%s" % (datetime.now().strftime("%d%m%Y-%H%M%S"), message_title.replace(" ", "_"))
     message_path = os.path.join(board_path, message_filename)
 
+    # Open the file (to have text written to)
     with open(message_path, "wt") as file:
+        # Write the message to the file
         file.write(message)
 
 
+# Function to hanle incoming requests
 def handle_request(request):
+    # If the request dictionary does not include a dictionary
+    if "command" not in request: return (False, None)
     response = ""
+    # GET_BOARDS request
     if request["command"] == "GET_BOARDS":
         response = json.dumps(get_boards())
+    # GET_MESSAGES request
     elif request["command"] == "GET_MESSAGES":
+        # Check all the required field is supplied
+        if "board_name" not in request: return (False, None)
         messages = get_messages(request["board_name"])[-100:]
         response = json.dumps(messages)
+    # POST_MESSAGE request
     elif request["command"] == "POST_MESSAGE":
+        # Check all the required fields are supplied
+        if not ("board_name" in request and "message_title" in request and "message" in request): return (False, None)
         create_message(request["board_name"], request["message_title"], request["message"])
         response = "[]"
-    else:
-        return None
-    return response
+    else: return (False, None)
+    return (True, response)
 
-try:
-    if not os.path.exists(BOARD_DIR):
-        os.makedirs(BOARD_DIR)
+# If the module is not being imported
+if __name__ == "__main__":
+    try:
+        # If the board directory does not exist, create it
+        if not os.path.exists(BOARD_DIR):
+            os.makedirs(BOARD_DIR)
 
-    print("Listening on %s:%d" % source)
-    sock = socket.socket()
-    sock.bind(source)
-    sock.listen(5)
+        # Create a TCP socket and bind to the provided host and port
+        print("Listening on %s:%d" % source)
+        sock = socket.socket()
+        sock.bind(source)
+        sock.listen(5)
 
-    while True:
-        connection, address = sock.accept()
-        print("Received connection from %s%d." % address)
-        request = json.loads(connection.recv(1024).decode("utf-8"))
-        response = handle_request(request)
-        if response is not None:
-            connection.send(response.encode("utf-8"))
-        log(address, request["command"], response is not None)
-        connection.close()
+        # Infinite loop to handle each request (and connection)
+        while True:
+            # Accept an incoming TCP connection
+            connection, address = sock.accept()
+            print("Received connection from %s%d." % address)
+            try:
+                request = json.loads(connection.recv(1024).decode("utf-8"))
+                success, response = handle_request(request)
+                if success: connection.send(response.encode("utf-8"))
+                log(address, request["command"], success)
+            except ValueError as e:
+                # A ValueError occurs when an empty string is provided to the JSON deserialiser
+                print("Error while decoding message:", e)
+            finally:
+                # Close the connection
+                connection.close()
 
-except OSError:
-    print("Error: Port %d already in use." % source[1])
+    except OSError as e:
+        # An OSError occurrs when an invalid host IP or port is supplied or a port has already been bound to
+        print("Error: Could not bind to %s, %s" % ("%s:%d" % source, e))
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt")
+    finally:
+        print("Exiting...")
